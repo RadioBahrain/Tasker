76dbe30484cf5b809f6909dc948423e835b5b09c
Hasan Aldoy - Info <aldoyh@info.gov.bh>
2025-06-02 15:06:15 +0300

    Add file existence check and creation for tasks_manifesto.md

--- .github/workflows/tasker.php
+++ .github/workflows/tasker.php
@@ -1,8 +1,21 @@
 <?php
 // update_tasks_manifesto.php
 
+/**
+ *
+ *
+ */
+
 $workspace = getenv('GITHUB_WORKSPACE');
 $filePath = $workspace . '/tasks_manifesto.md';
+
+if (!file_exists($filePath)) {
+    // Create the file if it doesn't exist
+    file_put_contents($filePath, "| Issue | Task | Status |\n|---|---|---|\n");
+    chmod($filePath, 0777);
+
+    echo "File created: $filePath\n";
+}
 
 $tasks = file_get_contents($filePath);
 $issueNumber = getenv('ISSUE_NUMBER');
